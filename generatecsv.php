29a1cf605771e5b847b68cbfc57f582d13d3b4c7
Elijah Cruz <ecwebservices@gmail.com>
2021-11-11 09:50:14 -0800

    Added comments to my code.

--- generatecsv.php
+++ generatecsv.php
@@ -2,14 +2,23 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+// Open/Create the file if doesn't exist, and give write permission to PHP
 $csv = fopen('generated.csv', "w");
 
+// Open a new Redis Connection via Predis
 $redis = new \Predis\Client();
 
-for($i = 0; $i <= 1000000; $i++) {
+// For the first 1 million rows
+for($i = 0; $i < 1000000; $i++) {
+    // Get the key from redis and decode as to an array.
     $array = json_decode($redis->get('csv.'.$i), true);
+    
+    // Add new line
     fputcsv($csv, $array);
+    
+    // CLI Echo
     echo 'Added Row ' . $i . PHP_EOL;
 }
 
+// Close the file
 fclose($csv);
--- redisfiller.php
+++ redisfiller.php
@@ -2,11 +2,17 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+// Using faker to generate the data.
 $faker = Faker\Factory::create();
 
+
+// Open Redis connection via Predis.
 $redis = new \Predis\Client();
 
-for($i = 0; $i <= 1000000; $i++) {
+// For the first 1 million keys
+for($i = 0; $i < 1000000; $i++) {
+
+    // Create a faker array.
     $array = [
         'name' => $faker->name,
         'email' => $faker->email,
@@ -15,9 +21,13 @@ for($i = 0; $i <= 1000000; $i++) {
         'random_numer' => random_int(0, 1000000),
         'random_string' => \Illuminate\Support\Str::random(random_int(16,256))
     ];
-
+    
+    // Json encode the array so we can store as one key per row.
     $json = json_encode($array);
 
+    // Add to redis
     $redis->set('csv.'.$i, $json);
+    
+    // CLI Output
     echo "Added key csv." . $i . PHP_EOL;
 }
